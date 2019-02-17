38f0f75cc8bee54f5d218399766def059223e062
Harish Sunkerikar <urhari04@gmail.com>
2019-02-17 22:44:46 +0530

    webcomponent with stencil js

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,37 @@
+import { Component, Prop } from "@stencil/core";
+
+@Component({
+	tag: "uc-side-drawer",
+	styleUrl: "./side-drawer.css",
+	shadow: true,
+})
+export class SideDrawer {
+	@Prop({ reflectToAttr: true }) title: string;
+
+	//@Prop() open: boolean;
+	render() {
+		// let content = null;
+		// if (this.open) {
+		// 	content = (
+		// 		<aside>
+		// 			<header>
+		// 				<h1>{this.title}</h1>
+		// 			</header>
+		// 			<main>
+		// 				<slot />
+		// 			</main>
+		// 		</aside>
+		// 	);
+		// }
+		return (
+			<aside>
+				<header>
+					<h1>{this.title}</h1>
+				</header>
+				<main>
+					<slot />
+				</main>
+			</aside>
+		);
+	}
+}
